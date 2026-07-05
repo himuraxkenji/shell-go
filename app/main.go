a659f6e812f7c90fb528628c675ec79f63a4bef7
himuraxkenji <aaron.ariperto@gmail.com>
2026-07-05 02:42:22 -0300

    feat: add type builtin with builtin command registry

--- app/main.go
+++ app/main.go
@@ -7,7 +7,17 @@ import (
 	"strings"
 )
 
-const EXIT = "exit"
+const (
+	EXIT = "exit"
+	ECHO = "echo"
+	TYPE = "type"
+)
+
+var builtinCommands = map[string]struct{}{
+	EXIT: {},
+	ECHO: {},
+	TYPE: {},
+}
 
 func main() {
 
@@ -22,9 +32,16 @@ func main() {
 			os.Exit(1)
 		}
 		command = strings.TrimSpace(command)
-		if command == EXIT {
+		if strings.HasPrefix(command, TYPE) {
+			if _, ok := builtinCommands[command[5:]]; ok {
+				fmt.Fprintln(os.Stdout, command[5:]+" is a shell builtin")
+			} else {
+				fmt.Fprintln(os.Stderr, command[5:]+": not found")
+			}
+
+		} else if command == EXIT {
 			break
-		} else if strings.HasPrefix(command, "echo") {
+		} else if strings.HasPrefix(command, ECHO) {
 			fmt.Fprintln(os.Stdout, command[5:])
 		} else {
 			fmt.Println(command + ": command not found")
