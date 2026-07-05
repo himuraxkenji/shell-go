a5973ec5eec5ae3df2243858ffc566ca22b88eaa
himuraxkenji <aaron.ariperto@gmail.com>
2026-07-05 01:22:11 -0300

    refactor: switch to bufio.Reader with input error handling

--- app/main.go
+++ app/main.go
@@ -7,12 +7,15 @@ import (
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Print("$ ")
-	if scanner.Scan() {
-		command := scanner.Text()
-		fmt.Printf("%s: command not found", command)
 
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading input", err)
+		os.Exit(1)
 	}
+
+	fmt.Println(command[:len(command)-1] + ": command not found")
 
 }
