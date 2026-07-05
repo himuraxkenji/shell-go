c90be4d7a98fd451734acead02c847d1f6865232
himuraxkenji <aaron.ariperto@gmail.com>
2026-07-05 01:14:03 -0300

    feat: read user input and report unknown commands

--- app/main.go
+++ app/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Print("$ ")
+	if scanner.Scan() {
+		command := scanner.Text()
+		fmt.Printf("%s: command not found", command)
+
+	}
+
 }
