95b68b7bacbafc57d7eacaf1bbf132b3a452c483
himuraxkenji <aaron.ariperto@gmail.com>
2026-07-05 01:44:56 -0300

    feat: add REPL loop for continuous command reading

--- app/main.go
+++ app/main.go
@@ -7,15 +7,19 @@ import (
 )
 
 func main() {
-	fmt.Print("$ ")
 
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	reader := bufio.NewReader(os.Stdin)
 
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading input", err)
-		os.Exit(1)
+	for {
+		fmt.Print("$ ")
+
+		command, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input", err)
+			os.Exit(1)
+		}
+		fmt.Println(command[:len(command)-1] + ": command not found")
+
 	}
-
-	fmt.Println(command[:len(command)-1] + ": command not found")
 
 }
